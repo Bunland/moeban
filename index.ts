2e10fb2d54b228216e6320a39ccad6a7a7ccaaf2
vitalspace <myglobalsounds@gmail.com>
2023-05-23 21:07:02 -0600

    Readme updated

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { Moeban } from "./src/lib";
 
 interface User {
-  _id: string;
+  _id: string | number;
   name: string;
   email: string;
 }
@@ -11,3 +11,14 @@ class UserModel extends Moeban<User> {
 }
 
 const userModel = new UserModel("example.json");
+
+const user: User = {
+  _id: 4,
+  name: "Eduardo",
+  email: "Eduardo@mail.com",
+};
+
+console.log(await userModel.write(JSON.stringify(user)));
+console.log(await userModel.find());
+console.log(await userModel.findOne("_id", "2"));
+console.log(await userModel.removeOne("_id", "2"));
